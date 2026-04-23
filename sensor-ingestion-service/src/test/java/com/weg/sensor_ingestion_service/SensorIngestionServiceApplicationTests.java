19568762122acec969fdf182f0c44b02e69f3bf5
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-04-23 16:33:49 -0300

    feat: adiciona projeto java do sensor-ingestions-service

---
+++ sensor-ingestion-service/src/test/java/com/weg/sensor_ingestion_service/SensorIngestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.sensor_ingestion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorIngestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
